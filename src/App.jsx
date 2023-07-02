4ab9a4b4c2fcfdac4b589420ddf46d5d0bd210c6
motazabdou <motazabdou30@gmail.com>
2023-07-02 14:14:01 +0100

    Created Vite app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <div>
+        Hello from App
+      </div>
+    </>
+  )
+}
+
+export default App
